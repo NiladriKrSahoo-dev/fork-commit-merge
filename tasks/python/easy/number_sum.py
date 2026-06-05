529ed54277a65306351380ef54ab8439ff158dfd
NiladriKrSahoo-dev <niladrikumarsahoo.26@gmail.com>
2026-06-06 00:46:34 +0530

    Update number_sum.py

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,5 @@
 # Python - Easy
-
+a= int(input("Enter first number:"))
+b= int(input("Enter second number:"))
+print("The sum of two numbers is:", a+b)
 # TODO: Write a function that returns the sum of two numbers
